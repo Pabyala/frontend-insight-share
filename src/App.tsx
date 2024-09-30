63b6fbc96556d1d48e36e2101e45dd53c6c790de
Eleomar Fajutnao <eleomarfaafajutnao@gmail.com>
2024-09-30 09:49:24 +0800

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
-import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto">
+      <h1 className="text-xs font-bold underline text-red-500">
+        Hello world! nice
+      </h1>
     </div>
   );
 }
