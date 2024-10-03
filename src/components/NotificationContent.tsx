572dc430845d4b44201eb47d83840700606b1ba6
Eleomar Fajutnao <eleomarfaafajutnao@gmail.com>
2024-10-03 16:36:12 +0800

    added custom icon and design notification content

--- src/components/NotificationContent.tsx
+++ src/components/NotificationContent.tsx
@@ -12,48 +12,67 @@ import { FaSadTear } from "react-icons/fa";
 import { FaFireAlt } from "react-icons/fa";
 import { RiEmotionHappyFill } from "react-icons/ri";
 import { FaFaceSurprise } from "react-icons/fa6";
-import { reactedToPost } from '../data/dummyData';
+import { notification, follower } from '../data/dummyData';
+import { Icon } from '@iconify/react';
+import { FluentPersonArrowBack24Filled, NotoAngryFace, NotoFaceWithTearsOfJoy, NotoOrangeHeart, NotoSadButRelievedFace, TwemojiFire, TwemojiRaisingHands } from './CustomIcons';
 
 export default function NotificationContent() {
-    const postContent = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)"
+
     return (
-        <div className="absolute right-0 mt-9 w-96 bg-white rounded-md shadow-lg py-2">
+        <div className="absolute right-0 mt-0 w-96 bg-white rounded-md shadow-lg py-2">
             <div className='block px-4 py-2 text-gray-800 '>
-                <h4>Notification</h4>
+                <h4 className='text-base'>Notification</h4>
                 <hr className="h-px mt-1 mb-1 bg-gray-200 border-0 dark:bg-gray-700" />
             </div>
-            {
-                reactedToPost.map((reactedPost) => (
-                    <div key={reactedPost.authorId} className='block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm'>
-                <div className="flex space-x-2">
-                    <div className="flex" style={{ margin: 'auto'}}>
-                        <Avatar
-                            sx={{ width: 38, height: 38 }}
-                            alt="Remy Sharp"
-                            src={reactedPost.profileURL}
-                        />
+            {notification.map((notif) => (
+                <div key={notif.notificationId} className={notif.isRead ? 'bg-gray-100 block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm cursor-pointer' : 'block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm cursor-pointer'}>
+                    <div className="flex justify-between space-x-2">
+                        <div className="profileAndDisc flex items-center space-x-2">
+                            <div className="flex" style={{ margin: 'auto' }}>
+                                <Avatar
+                                    sx={{ width: 38, height: 38 }}
+                                    alt="Remy Sharp"
+                                    src={notif.profileURL}
+                                />
+                            </div>
+                            <div>
+                                {notif.notificationType === 'reacted' && (
+                                    <>
+                                        <span className='font-bold text-sm'>{notif.firstName} {notif.middleName} {notif.lastName}</span>
+                                        <span className='text-sm'> reacted to your post: "{notif.postContent.slice(0, 33)}..."</span>
+                                    </>
+                                )}
+
+                                {notif.notificationType === 'following' && (
+                                    <>
+                                        <span className='font-bold text-sm'>{notif.firstName} {notif.middleName} {notif.lastName} </span>
+                                        <span>stated following you</span>
+                                    </>
+                                )}
+
+                            </div>
+                        </div>
+                        <div>
+                            {notif.notificationType === 'reacted' && (
+                                <>
+                                    {notif.reaction === 'hands-up' && <TwemojiRaisingHands className="text-4xl" />}
+                                    {notif.reaction === 'love' && <NotoOrangeHeart className="text-red-600 text-4xl text-blue-600/100" />}
+                                    {notif.reaction === 'haha' && <NotoFaceWithTearsOfJoy className="text-yellow-700 text-4xl text-blue-600/100" />}
+                                    {notif.reaction === 'fire' && <TwemojiFire className="text-yellow-400 text-4xl text-blue-600/100" />}
+                                    {notif.reaction === 'sad' && <NotoSadButRelievedFace className="text-red-400 text-4xl text-blue-600/100" />}
+                                    {notif.reaction === 'angry' && <NotoAngryFace className="text-orange-500 text-4xl text-blue-600/100" />}
+                                </>
+                            )}
+                            {notif.notificationType === 'following' && (
+                                <FluentPersonArrowBack24Filled className="text-4xl cursor-pointer" />
+                            )}
+                        </div>
                     </div>
-                    <span>
-                        <span className='font-bold text-sm'>{reactedPost.firstName} {reactedPost.middleName} {reactedPost.lastName}</span>
-                        <span className='text-sm'> {reactedPost.reaction} </span>
-                        <span className='text-sm'>your post: </span>
-                        <span className='text-sm'>"{reactedPost.postContent.slice(0, 40)}..."</span>
-                    </span>
-                    <div>
-                        {reactedPost.reaction === 'like' &&  <AiFillLike className="text-gray-400 text-4xl text-blue-600/100" />}
-                        {reactedPost.reaction === 'love' &&  <FaHeart className="text-red-600 text-4xl text-blue-600/100" />}
-                        {reactedPost.reaction === 'haha' &&  <RiEmotionHappyFill className="text-yellow-700 text-4xl text-blue-600/100" />}
-                        {reactedPost.reaction === 'wow' &&  <FaFaceSurprise className="text-yellow-400 text-4xl text-blue-600/100" />}
-                        {reactedPost.reaction === 'sad' &&  <FaSadTear className="text-red-400 text-4xl text-blue-600/100" />}
-                        {reactedPost.reaction === 'angry' &&  <FaAngry className="text-orange-500 text-4xl text-blue-600/100" />}
+                    <div className=''>
+                        <span className="text-gray-400 text-center text-xs">{notif.timeStamp}</span>
                     </div>
                 </div>
-                <div className=''>
-                    <span className="text-gray-400 text-center text-xs">{reactedPost.timeStamp}</span>
-                </div>
-            </div>
-                ))
-            }
+            ))}
         </div>
     )
 }
--- src/components/SubmenuProfile.tsx
+++ src/components/SubmenuProfile.tsx
@@ -1,20 +1,15 @@
 import { Avatar } from '@mui/material'
 import React from 'react'
+import { AntDesignSettingFilled, ClarityUserSolid, IonLogOut } from './CustomIcons'
 
 export default function SubmenuProfile() {
     return (
         <div className="absolute right-0 mt-9 w-52 bg-white rounded-md shadow-lg py-2">
             <div className='block px-4 py-2 text-gray-800 hover:bg-gray-100'>
                 <div className="flex">
-                    {/* <img
-                        alt=""
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        className="h-8 w-8 rounded-full"
-                    /> */}
                     <Avatar
                         sx={{ width: 32, height: 32 }}
                         alt="Remy Sharp"
-                        // src="/static/images/avatar/1.jpg" 
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     />
                     <div className='flex flex-col ml-2.5'>
@@ -27,9 +22,30 @@ export default function SubmenuProfile() {
                 <hr className="h-px mt-1 mb-1 bg-gray-200 border-0 dark:bg-gray-700" />
             </div>
 
-            <a href="/" className="block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm" > Your Profile </a>
-            <a href="/" className="block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm" > Settings </a>
-            <a href="/" className="block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm" > Logout </a>
+            <a href="/" className="block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm" >
+                <div className='flex items-center space-x-2'>
+                    <div>
+                        <ClarityUserSolid />
+                    </div>
+                    <span>Your Profile </span>
+                </div>
+            </a>
+            <a href="/" className="block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm" >
+                <div className='flex items-center space-x-2'>
+                    <div>
+                        <AntDesignSettingFilled />
+                    </div>
+                    <span>Settings</span>
+                </div>
+            </a>
+            <a href="/" className="block px-4 py-1.5 text-xs text-gray-800 hover:bg-gray-100 lg:text-sm" >
+                <div className='flex items-center space-x-2'>
+                    <div>
+                        <IonLogOut />
+                    </div>
+                    <span>Logout</span>
+                </div>
+            </a>
         </div>
     )
 }
